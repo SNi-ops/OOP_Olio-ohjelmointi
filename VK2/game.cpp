2ad85c91aa080cfb1d6c3e652083e127a84b2559
SNi-ops <santtu.nissinen99@gmail.com>
2026-01-16 15:39:14 +0200

    Valmis

---
+++ VK2/game.cpp
@@ -0,0 +1,42 @@
+#include "game.h"
+#include<ctime>
+#include <iostream>
+using namespace std;
+
+
+Game::Game(int maxNum){
+    maxNumber = maxNum;
+
+    srand(time(0));
+    randomNumber = rand() % maxNumber +1;
+
+    cout << "GAME CONSTRUKTOR: Game initalized with " <<maxNumber<< " as a max value."<<endl;
+}
+
+Game::~Game(){
+    cout<<"GAME DESTROYED!"<<endl;
+}
+
+void Game::play(){
+    while(randomNumber != playerGuess){
+        numOfGuesses++;
+
+        cout<<"Guess number between 1-"<<maxNumber<<":";
+        cin >> playerGuess;
+
+        if (playerGuess < randomNumber){
+            cout << "Your guess was too small!" << endl;
+        }
+        else if(playerGuess > randomNumber){
+            cout << "Your guess was too big!" << endl;
+        }
+        else if(playerGuess == randomNumber){
+            cout << "Your guess was RIGHT!" << endl;
+        }
+    }
+    printGameResult();
+}
+
+void Game::printGameResult(){
+    cout<< "Your guess: "<<playerGuess<< " was right and you guessed: "<<numOfGuesses<<" times."<<endl;
+}
---
+++ VK2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "game.h"
+
+using namespace std;
+
+int main()
+{
+    Game peli(40);
+    peli.play();
+    return 0;
+}
